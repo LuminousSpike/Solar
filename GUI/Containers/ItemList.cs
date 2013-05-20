42176052536226b87aae66f27874fe8f268bb3da
Nathan <nathan.j.todd@gmail.com>
2013-05-20 16:09:27 +1200

    Created ItemList Class.

---
+++ GUI/Containers/ItemList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solar.GUI.Containers
+{
+    class ItemList
+    {
+        public void Initialize()
+        {
+
+        }
+
+        public void LoadContent()
+        {
+
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
