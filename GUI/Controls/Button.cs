d392f47599d63a487fdbbd01cc3b3b3efe07039d
Nathan <nathan.j.todd@gmail.com>
2013-05-25 19:11:03 +1200

    Started cleaning up Button Class code.

--- GUI/Controls/Button.cs
+++ GUI/Controls/Button.cs
@@ -6,8 +6,8 @@ namespace Solar.GUI.Controls
 {
     public class Button
     {
-        private Vector2 position, Scale, TextPosition;
-        private Texture2D MainTexture, HoverTexture, SelectedTexture;
+        private Vector2 position, scale, textPosition;
+        private Texture2D mainTexture, HoverTexture, SelectedTexture;
         private Box DefaultBox, HoverBox, SelectedBox;
         private int Width, Height, BWidth;
         private Color MColor, MColorSelected, BColor, FontColor = Color.White;
@@ -44,7 +44,7 @@ namespace Solar.GUI.Controls
             Width = width;
             Height = height;
             BWidth = bWidth;
-            Scale = new Vector2(width - bWidth, height - bWidth);
+            scale = new Vector2(width - bWidth, height - bWidth);
             MColor = mColor;
             MColorSelected = mColorSelected;
             BColor = bColor;
@@ -61,17 +61,17 @@ namespace Solar.GUI.Controls
             {
                 DefaultBox = new Box(position, Width, Height, BWidth, MColor, BColor, graphicsdevice);
                 SelectedBox = new Box(position, Width, Height, BWidth, MColorSelected, BColor, graphicsdevice);
-                TextPosition = new Vector2((int)(position.X + (Width / 2) - (Font.MeasureString(Text).X / 2)), (int)(position.Y + (Height / 2) - (Font.MeasureString(Text).Y / 2)));
+                textPosition = new Vector2((int)(position.X + (Width / 2) - (Font.MeasureString(Text).X / 2)), (int)(position.Y + (Height / 2) - (Font.MeasureString(Text).Y / 2)));
             }
             else
             {
-                MainTexture = content.Load<Texture2D>(TextureMainPath);
+                mainTexture = content.Load<Texture2D>(TextureMainPath);
                 SelectedTexture = content.Load<Texture2D>(TextureSelectedPath);
-                CurrentTexture = MainTexture;
-                TextPosition = new Vector2((int)(position.X + (CurrentTexture.Width / 2) - (Font.MeasureString(Text).X / 2)), (int)(position.Y + (CurrentTexture.Height / 2) - (Font.MeasureString(Text).Y / 2)));
+                CurrentTexture = mainTexture;
+                textPosition = new Vector2((int)(position.X + (CurrentTexture.Width / 2) - (Font.MeasureString(Text).X / 2)), (int)(position.Y + (CurrentTexture.Height / 2) - (Font.MeasureString(Text).Y / 2)));
             }
 
-            rectangle = new Rectangle((int)position.X, (int)position.Y, MainTexture.Width, MainTexture.Height);
+            rectangle = new Rectangle((int)position.X, (int)position.Y, mainTexture.Width, mainTexture.Height);
         }
 
         public void UnloadContent()
@@ -89,13 +89,13 @@ namespace Solar.GUI.Controls
             {
                 if (Selected)
                 {
-                    spritebatch.Draw(SelectedTexture, position, null, Color.White, 0f, new Vector2(MainTexture.Width / 2, MainTexture.Height / 2), 1.0f, SpriteEffects.None, 0);
+                    spritebatch.Draw(SelectedTexture, position, null, Color.White, 0f, new Vector2(mainTexture.Width / 2, mainTexture.Height / 2), 1.0f, SpriteEffects.None, 0);
                 }
                 else
                 {
-                    spritebatch.Draw(CurrentTexture, position, null, Color.White, 0f, new Vector2(MainTexture.Width / 2, MainTexture.Height / 2), 1.0f, SpriteEffects.None, 0);
+                    spritebatch.Draw(CurrentTexture, position, null, Color.White, 0f, new Vector2(mainTexture.Width / 2, mainTexture.Height / 2), 1.0f, SpriteEffects.None, 0);
                 }
-                spritebatch.DrawString(Font, Text, TextPosition, FontColor, 0f, new Vector2(MainTexture.Width / 2, MainTexture.Height / 2), 1.0f, SpriteEffects.None, 0);
+                spritebatch.DrawString(Font, Text, textPosition, FontColor, 0f, new Vector2(mainTexture.Width / 2, mainTexture.Height / 2), 1.0f, SpriteEffects.None, 0);
             }
             else
             {
@@ -103,7 +103,7 @@ namespace Solar.GUI.Controls
                     SelectedBox.Draw(spritebatch);
                 else
                     DefaultBox.Draw(spritebatch);
-                spritebatch.DrawString(Font, Text, TextPosition, FontColor);
+                spritebatch.DrawString(Font, Text, textPosition, FontColor);
             }
         }
     }
